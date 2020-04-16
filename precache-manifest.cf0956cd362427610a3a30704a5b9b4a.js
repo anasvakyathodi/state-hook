27d676b0bf3e456aab7ec7421bfd6204083fd5c9
anasvakyathodi <avtvld@gmail.com>
2020-04-16 20:00:25 +0530

    Updates

---
+++ precache-manifest.cf0956cd362427610a3a30704a5b9b4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1efa149e8a6d562c759da0cfe54195",
+    "url": "/state-hook/index.html"
+  },
+  {
+    "revision": "3d7aefbaa5dc60e72318",
+    "url": "/state-hook/static/css/main.f8aacc0d.chunk.css"
+  },
+  {
+    "revision": "886e3b08254f0fda6252",
+    "url": "/state-hook/static/js/2.a64c7892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/state-hook/static/js/2.a64c7892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7aefbaa5dc60e72318",
+    "url": "/state-hook/static/js/main.1f0ef656.chunk.js"
+  },
+  {
+    "revision": "d43fc20a654ad2ca2f93",
+    "url": "/state-hook/static/js/runtime-main.b9344946.js"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/state-hook/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/state-hook/static/media/map-image.f40ed5b1.png"
+  }
+]);
